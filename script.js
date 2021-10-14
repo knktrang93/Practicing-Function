27d13cc4e76a3b0711fcec6e71327690ab5e4f35
knktrang93 <87623875+knktrang93@users.noreply.github.com>
2021-10-14 14:53:56 -0700

    added index and js

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const flight = "LH234";
+const trang = {
+  name: "Trang Khong",
+  passport: 233450005959,
+};
+
+const checkIn = function (flightNumber, passenger) {
+  flightNum = "LH999";
+};
+
+checkIn(flight, trang);
