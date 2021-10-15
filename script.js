aac53a4df5d951f9fb1989c2ab3373445e974b51
knktrang93 <87623875+knktrang93@users.noreply.github.com>
2021-10-15 15:19:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,35 @@
 
 // greet("Helloo")("Trang");
 
-//arrow function
+//change to arrow function
 
-const greet = (greeting) => (name) => console.log(`${greeting} ${name}`);
+// const greet = (greeting) => (name) => console.log(`${greeting} ${name}`);
 
-greet("Helloo")("Trang");
+// greet("Helloo")("Trang");
+
+//--------------------------------------------------
+
+const southWest = {
+  airline: "South West",
+  iataCode: "SW",
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}, name` });
+  },
+};
+
+southWest.book(239, "Trang Khong");
+southWest.book(567, "Tra Khong");
+
+const american = {
+  airline: "American",
+  iataCode: "AA",
+  bookings: [],
+};
+
+const book = southWest.book;
+
+book(123, "Truc Khong");
